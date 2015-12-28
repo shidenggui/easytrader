8adef2559c7e60cdaf9dc28e02504520d120443d
shidenggui <longlyshidenggui@gmail.com>
2015-12-28 21:10:45 +0800

    添加简单的单元测试

---
+++ test_easytrader.py
@@ -0,0 +1,31 @@
+import unittest
+import easytrader
+from easytrader import helpers
+
+class TestEasytrader(unittest.TestCase):
+    def test_helpers(self):
+        result = helpers.get_stock_type('162411')
+        self.assertEqual(result, 'sz')
+
+        result = helpers.get_stock_type('691777')
+        self.assertEqual(result, 'sh')
+
+        result = helpers.get_stock_type(162411)
+        self.assertEqual(result, 'sz')
+
+    def test_format_response_data_type(self):
+        user = easytrader.use('ht')
+
+        test_data = [{
+            'current_amount': '187.00',
+            'current_balance': '200.03',
+            'stock_code' : '000001'
+        }]
+        result = user.format_response_data_type(test_data)
+
+        self.assertIs(type(result[0]['current_amount']), int)
+        self.assertIs(type(result[0]['current_balance']), float)
+        self.assertIs(type(result[0]['stock_code']), str)
+
+if __name__ == '__main__':
+    unittest.main()
