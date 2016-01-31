ed849eb8ae7e9ab2f55fe4f3679f432fda12fdda
shidenggui <longlyshidenggui@gmail.com>
2016-01-31 17:29:40 +0800

    feat(http): add http server

---
+++ httpserver.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from flask import Flask, request, jsonify
+import anyjson as json
+
+import easytrader
+
+
+app = Flask(__name__)
+user = None
+
+
+@app.route('/login')
+def login():
+    global user
+    args = request.args
+    user = easytrader.use(args['use'])
+    user.prepare(args['prepare'])
+    return jsonify({'message': 'login ok'})
+
+
+@app.route('/call')
+def do():
+    global user
+    target = request.args.get('func')
+    params_str = request.args.get('params', None)
+    params = params_str.split(',')
+
+    if params:
+        result = getattr(user, target)(**params)
+    else:
+        result = getattr(user, target)
+    return json.dumps(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
