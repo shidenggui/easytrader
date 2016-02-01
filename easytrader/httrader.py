f53727c5acceba97f7f242b6bbe90be86d903512
shidenggui <longlyshidenggui@gmail.com>
2016-02-01 21:38:19 +0800

    fix(py27): from future import division

--- easytrader/httrader.py
+++ easytrader/httrader.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from __future__ import division
 import base64
 import json
 import os
--- easytrader/yjbtrader.py
+++ easytrader/yjbtrader.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from __future__ import division
 import json
 import os
 import random
