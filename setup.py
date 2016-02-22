0fc2f536e9b9167e30a844dbbe962958b1353df4
shidenggui <longlyshidenggui@gmail.com>
2016-02-22 22:10:30 +0800

    refactor

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-from setuptools import setup, find_packages
-import codecs
-import os
+from setuptools import setup
+
 import easytrader
 
-
 setup(
-    name='easytrader',
-    version=easytrader.__version__,
-    description='A utility for China Stock Trade',
-    author='shidenggui',
-    author_email='longlyshidenggui@gmail.com',
-    license='BSD',
-    url='https://github.com/shidenggui/easytrader',
-    keywords='China stock trade',
-    classifiers=['Development Status :: 4 - Beta',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'License :: OSI Approved :: BSD License'],
-    packages=['easytrader', 'easytrader.config', 'easytrader.thirdlibrary'],
-    package_data={'': ['*.jar', '*.json'], 'config': ['config/*.json'], 'thirdlibrary': ['thirdlibrary/*.jar']},
+        name='easytrader',
+        version=easytrader.__version__,
+        description='A utility for China Stock Trade',
+        author='shidenggui',
+        author_email='longlyshidenggui@gmail.com',
+        license='BSD',
+        url='https://github.com/shidenggui/easytrader',
+        keywords='China stock trade',
+        classifiers=['Development Status :: 4 - Beta',
+                     'Programming Language :: Python :: 2.6',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.2',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'License :: OSI Approved :: BSD License'],
+        packages=['easytrader', 'easytrader.config', 'easytrader.thirdlibrary'],
+        package_data={'': ['*.jar', '*.json'], 'config': ['config/*.json'], 'thirdlibrary': ['thirdlibrary/*.jar']},
 )
