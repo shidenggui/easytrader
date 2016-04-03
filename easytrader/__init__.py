8608fe60726bb8a20b408f9a348a28e631d8ca14
shidenggui <longlyshidenggui@gmail.com>
2016-04-03 08:22:42 +0800

    fix(helpers): fix stock code decide

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -5,5 +5,5 @@ from .webtrader import WebTrader
 from .yhtrader import YHTrader
 from .yjbtrader import YJBTrader
 
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 __author__ = 'shidenggui'
--- easytrader/helpers.py
+++ easytrader/helpers.py
@@ -44,9 +44,23 @@ def file2dict(path):
 
 def get_stock_type(stock_code):
     """判断股票ID对应的证券市场
-    :param stock_code:股票ID
+    匹配规则
+    ['50', '51', '60', '90', '110'] 为 sh
+    ['00', '13', '18', '15', '16', '18', '20', '30', '39', '115'] 为 sz
+    ['5', '6', '9'] 开头的为 sh， 其余为 sz
+    :param stock_code:股票ID, 若以 'sz', 'sh' 开头直接返回对应类型，否则使用内置规则判断
     :return 'sh' or 'sz'"""
-    if str(stock_code).startswith(('5', '6', '9','11','13')):
+    assert type(stock_code) is str, 'stock code need str type'
+    if stock_code.startswith(('sh', 'sz')):
+        return stock_code[:2]
+    if stock_code.startswith(('50', '51', '60', '90', '110')):
+        return 'sh'
+    if stock_code.startswith(('00', '13', '18', '15', '16', '18', '20', '30', '39', '115')):
+        return 'sz'
+    log.warn(
+            'cant auto decide {code} stock type, use default simple rule, please manually use sz{code}/sh{code}'.format(
+                    code=stock_code))
+    if stock_code.startswith(('5', '6', '9')):
         return 'sh'
     return 'sz'
 
@@ -71,7 +85,8 @@ def recognize_verify_code(image_path, broker='ht'):
                     pipe = Popen(cmd, shell=True, cwd=cwd, env=env, stdout=PIPE, stderr=STDOUT)
                     (output, errout) = pipe.communicate(input=input)
                     return output.decode().rstrip('\n')
-                getcmdout_func = lambda:_
+
+                getcmdout_func = lambda: _
                 getcmdout_func.getoutput = get_status_output
             else:
                 import commands
@@ -82,7 +97,7 @@ def recognize_verify_code(image_path, broker='ht'):
         log.debug('java detect result: %s' % out_put)
         if out_put.find('java version') != -1 or out_put.find('openjdk') != -1:
             tool_path = os.path.join(os.path.dirname(__file__), 'thirdlibrary', verify_code_tool)
-            out_put = getcmdout_func.getoutput('java -jar "{}" {} {}'.format(tool_path , param, image_path))
+            out_put = getcmdout_func.getoutput('java -jar "{}" {} {}'.format(tool_path, param, image_path))
             log.debug('recognize output: %s' % out_put)
             verify_code_start = -4
             return out_put[verify_code_start:]
