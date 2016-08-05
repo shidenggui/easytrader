5a6a53fa41007b86870fe78e40f978a2ec1bd575
feifei <haogefeifei@qq.com>
2016-08-05 10:38:03 +0800

    优化了佣金宝查询ipo限制的代码, 修改了父类接口名

--- easytrader/yjbtrader.py
+++ easytrader/yjbtrader.py
@@ -151,22 +151,17 @@ class YJBTrader(WebTrader):
         :param stock_code: 申购代码!!!
         :return: high_amount(最高申购股数) enable_amount(申购额度) last_price(发行价)
         """
-        if not self.heart_thread.is_alive():
-            check_data = self.get_balance()
-            if type(check_data) == dict:
-                return check_data
         need_info = self.__get_trade_need_info(stock_code)
         params = dict(
+                self.config['ipo_enable_amount'],
                 CSRF_Token='undefined',
                 timestamp=random.random(),
-                request_id=self.config['buystock'],
                 stock_account=need_info['stock_account'],  # '沪深帐号'
                 exchange_type=need_info['exchange_type'],  # '沪市1 深市2'
                 entrust_prop=0,
                 stock_code=stock_code
         )
-        return_json = self.format_response_data(self.request(params))
-        data = self.fix_error_data(return_json)
+        data = self.do(params)
         if 'error_no' in data.keys() and data['error_no'] != "0":
             log.debug('查询错误: %s' % (data['error_info']))
             return None
