becdc3e6aa1b79cd3bde63b40f0890ad3bbf847f
周炀 <zhoudayang2@163.com>
2016-08-08 16:28:20 +0800

    修改支持多进程 (#94)
    * change to support multi process
    
    * change to support multiprocess

--- easytrader/httrader.py
+++ easytrader/httrader.py
@@ -90,7 +90,7 @@ class HTTrader(WebTrader):
         # 获取验证码
         verify_code_response = self.s.get(self.config['verify_code_api'])
         # 保存验证码
-        image_path = os.path.join(tempfile.gettempdir(), 'vcode')
+        image_path = os.path.join(tempfile.gettempdir(), 'vcode_%d'%os.getpid())
         with open(image_path, 'wb') as f:
             f.write(verify_code_response.content)
 
