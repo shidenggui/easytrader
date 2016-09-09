11a1e01da9470ce029e32cb80c5f4706720f4a2d
shanghaitong <shanghaitong@163.com>
2016-09-09 17:43:23 +0800

    Update log.py

--- easytrader/log.py
+++ easytrader/log.py
@@ -3,6 +3,7 @@ import logging
 
 log = logging.getLogger('easytrader')
 log.setLevel(logging.DEBUG)
+log.propagate = False
 
 fmt = logging.Formatter('%(asctime)s [%(levelname)s] %(filename)s %(lineno)s: %(message)s')
 ch = logging.StreamHandler()
