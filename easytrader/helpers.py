452ac5c4214ca557dbdad3352fa4f865424e3ed5
feifei <haogefeifei@qq.com>
2016-09-13 18:50:13 +0800

    增加查询今天可以申购的新股信息方法

--- easytrader/helpers.py
+++ easytrader/helpers.py
@@ -198,3 +198,63 @@ def get_30_date():
     end_date = now.date()
     start_date = end_date - datetime.timedelta(days=30)
     return start_date.strftime("%Y%m%d"), end_date.strftime("%Y%m%d")
+
+
+def get_today_ipo_data():
+    """
+    查询今天可以申购的新股信息
+    :return: 今日可申购新股列表 apply_code申购代码 price发行价格
+    """
+
+    import random
+    import json
+    import datetime
+    import six
+
+    if six.PY2:
+        import urllib2
+    else:
+        import urllib
+
+    send_headers = {
+        'Host': 'xueqiu.com',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:43.0) Gecko/20100101 Firefox/43.0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'deflate',
+        'Cache-Control': 'no-cache',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Referer': 'https://xueqiu.com/hq',
+        'Cookie': 's=2u40123a0t; Hm_lvt_1db88642e346389874251b5a1eded6e3=1473758386; xq_a_token=b8d18e01dc28a758f'
+                  'd4659f5c100f1eafaddde13; xq_r_token=6d8d9fc5cfd2c5537575644172bbf81be2c86321; Hm_lpvt_1db88642e'
+                  '346389874251b5a1eded6e3=1473758409; __utma=1.119845140.1473758388.1473758388.1473758388.1; __utmb'
+                  '=1.2.10.1473758388; __utmc=1; __utmz=1.1473758388.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(no'
+                  'ne); __utmt=1',
+        'Connection': 'keep-alive'
+    }
+
+    sj = random.randint(1000000000000, 9999999999999)
+    url = "https://xueqiu.com/proipo/query.json?column=symbol,name,onl_subcode,onl_subbegdate,actissqty,onl_actissq" \
+          "ty,onl_submaxqty,iss_price,onl_lotwiner_stpub_date,onl_lotwinrt,onl_lotwin_amount,stock_income&orderBy=" \
+          "onl_subbegdate&order=desc&stockType=&page=1&size=10&_=%s" % (str(sj))
+    if six.PY2:
+        req = urllib2.Request(url, headers=send_headers)
+        html = urllib2.urlopen(req).read().decode('UTF-8')
+    else:
+        req = urllib.request.Request(url, headers=send_headers)
+        html = urllib.request.urlopen(req).read().decode('UTF-8')
+
+    json_data = json.loads(html)
+    today_ipo = []
+
+    for line in json_data['data']:
+        # if datetime.datetime(2016, 9, 14).ctime()[:10] == line[3][:10]:
+        if datetime.datetime.now().ctime()[:10] == line[3][:10]:
+            today_ipo.append({
+                'stock_code': line[0],
+                'stock_name': line[1],
+                'apply_code': line[2],
+                'price': line[7]
+            })
+
+    return today_ipo
