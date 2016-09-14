7866dfd8bf791ef3b0b28b20e35278edbaff4add
sxhuweijun <sxhuweijun@163.com>
2016-09-14 21:10:02 +0800

    增加三个查询新功能，今日委托列表，今日新股申购信息，今日账户新股申购额度

--- easytrader/gftrader.py
+++ easytrader/gftrader.py
@@ -437,3 +437,32 @@ class GFTrader(WebTrader):
             "end_date": end_date,
         })
         return self.do(params)
+        
+    @property
+    def today_ipo_list(self):
+        '''
+
+        查询今日ipo的股票列表
+        :return:
+        '''
+        params = self.config['today_ipo_list'].copy()
+        return self.do(params)
+
+    @property
+    def today_entrust(self):
+        '''
+
+        查询今日委托列表
+        :return:
+        '''
+        params = self.config['today_entrust'].copy()
+        return self.do(params)
+
+    def today_ipo_limit(self):
+        '''
+
+        查询今日账户新股申购额度
+        :return:
+        '''
+        params = self.config['today_ipo_limit'].copy()
+        return self.do(params)
