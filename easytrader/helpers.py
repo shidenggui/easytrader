d18527d74ee61565135b337311e5c7b9d9c44ee0
haogefeifei <haogefeifei@qq.com>
2016-11-03 14:02:59 +0800

    改下写法

--- easytrader/helpers.py
+++ easytrader/helpers.py
@@ -263,9 +263,7 @@ def get_today_ipo_data():
 
     for line in json_data['data']:
         # if datetime.datetime(2016, 9, 14).ctime()[:10] == line[3][:10]:
-        day = datetime.datetime.now().ctime()[:10]
-        day = day if day[8] == ' ' else day[:8] + '0' + day[9:]
-        if day == line[3][:10]:
+        if datetime.datetime.now().strftime('%a %b %d') == line[3][:10]:
             today_ipo.append({
                 'stock_code': line[0],
                 'stock_name': line[1],
