b77e748e4e41a0673310ee7870795e56f5eabb96
shidenggui <longlyshidenggui@gmail.com>
2016-12-21 07:17:51 +0800

    fix py2 str compatible

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -7,5 +7,5 @@ from .yjbtrader import YJBTrader
 from .gftrader import GFTrader
 from .joinquant_follower import JoinQuantFollower
 
-__version__ = '0.10.1'
+__version__ = '0.10.2'
 __author__ = 'shidenggui'
