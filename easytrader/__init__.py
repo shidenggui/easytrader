92fa4281c49c924c696647773c94b4f732dc45c4
shidenggui <903618848@qq.com>
2017-03-08 17:10:57 +0800

    update version

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -8,5 +8,5 @@ from .gftrader import GFTrader
 from .joinquant_follower import JoinQuantFollower
 from .ricequant_follower import RiceQuantFollower
 
-__version__ = '0.11.6'
+__version__ = '0.11.7'
 __author__ = 'shidenggui'
