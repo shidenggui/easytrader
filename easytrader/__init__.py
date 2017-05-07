59868b9bdc5af866371a8bf157a2806166aed38c
shidenggui <903618848@qq.com>
2017-05-07 22:37:58 +0800

    fix bug

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -6,5 +6,5 @@ from .gftrader import GFTrader
 from .joinquant_follower import JoinQuantFollower
 from .ricequant_follower import RiceQuantFollower
 
-__version__ = '0.11.16'
+__version__ = '0.11.17'
 __author__ = 'shidenggui'
