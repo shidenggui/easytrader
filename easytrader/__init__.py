7bbd1fbfcf157e0bac17936e842c2fafcc838a3a
shidenggui <903618848@qq.com>
2017-09-10 20:27:45 +0800

    add more sleep when close after start dialog to get proper top_window handle

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -6,5 +6,5 @@ from .joinquant_follower import JoinQuantFollower
 from .ricequant_follower import RiceQuantFollower
 from . import exceptions
 
-__version__ = '0.12.1'
+__version__ = '0.12.2'
 __author__ = 'shidenggui'
