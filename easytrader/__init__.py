4429b77bd05c4759c79a95fb80acda1e6e0fa0ac
shidenggui <903618848@qq.com>
2018-01-08 15:24:58 +0800

    update version

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -6,5 +6,5 @@ from .joinquant_follower import JoinQuantFollower
 from .ricequant_follower import RiceQuantFollower
 from . import exceptions
 
-__version__ = '0.12.3'
+__version__ = '0.12.4'
 __author__ = 'shidenggui'
