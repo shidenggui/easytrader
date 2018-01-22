f15ebc7f15c41578c3e0cab044f8f0e55f6663ce
shidenggui <903618848@qq.com>
2018-01-22 23:13:15 +0800

    refactor: server use decorater replace duplicated code

--- easytrader/server.py
+++ easytrader/server.py
@@ -1,135 +1,131 @@
+import functools
+
 from flask import Flask, request, jsonify
 
 from . import api
+from .log import log
 
 app = Flask(__name__)
 
 global_store = {}
 
 
+def error_handle(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            log.exception('server error')
+            message = '{}: {}'.format(e.__class__, e)
+            return jsonify({'error': message}), 400
+
+    return wrapper
+
+
 @app.route('/prepare', methods=['POST'])
+@error_handle
 def post_prepare():
     json_data = request.get_json(force=True)
 
-    try:
-        user = api.use(json_data.pop('broker'))
-        user.prepare(**json_data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = api.use(json_data.pop('broker'))
+    user.prepare(**json_data)
 
     global_store['user'] = user
     return jsonify({'msg': 'login success'}), 201
 
 
 @app.route('/balance', methods=['GET'])
+@error_handle
 def get_balance():
-    try:
-        user = global_store['user']
-        balance = user.balance
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    balance = user.balance
 
     return jsonify(balance), 200
 
 
 @app.route('/position', methods=['GET'])
+@error_handle
 def get_position():
-    try:
-        user = global_store['user']
-        position = user.position
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    position = user.position
 
     return jsonify(position), 200
 
 
 @app.route('/auto_ipo', methods=['GET'])
+@error_handle
 def get_auto_ipo():
-    try:
-        user = global_store['user']
-        res = user.auto_ipo()
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    res = user.auto_ipo()
 
     return jsonify(res), 200
 
 
 @app.route('/today_entrusts', methods=['GET'])
+@error_handle
 def get_today_entrusts():
-    try:
-        user = global_store['user']
-        today_entrusts = user.today_entrusts
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    today_entrusts = user.today_entrusts
 
     return jsonify(today_entrusts), 200
 
 
 @app.route('/today_trades', methods=['GET'])
+@error_handle
 def get_today_trades():
-    try:
-        user = global_store['user']
-        today_trades = user.today_trades
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    today_trades = user.today_trades
 
     return jsonify(today_trades), 200
 
 
 @app.route('/cancel_entrusts', methods=['GET'])
+@error_handle
 def get_cancel_entrusts():
-    try:
-        user = global_store['user']
-        cancel_entrusts = user.cancel_entrusts
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    cancel_entrusts = user.cancel_entrusts
 
     return jsonify(cancel_entrusts), 200
 
 
 @app.route('/buy', methods=['POST'])
+@error_handle
 def post_buy():
     json_data = request.get_json(force=True)
-    try:
-        user = global_store['user']
-        res = user.buy(**json_data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    res = user.buy(**json_data)
 
     return jsonify(res), 201
 
 
 @app.route('/sell', methods=['POST'])
+@error_handle
 def post_sell():
     json_data = request.get_json(force=True)
-    try:
-        user = global_store['user']
-        res = user.sell(**json_data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+
+    user = global_store['user']
+    res = user.sell(**json_data)
 
     return jsonify(res), 201
 
 
 @app.route('/cancel_entrust', methods=['POST'])
+@error_handle
 def post_cancel_entrust():
     json_data = request.get_json(force=True)
-    try:
-        user = global_store['user']
-        res = user.cancel_entrust(**json_data)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+
+    user = global_store['user']
+    res = user.cancel_entrust(**json_data)
 
     return jsonify(res), 201
 
 
 @app.route('/exit', methods=['GET'])
+@error_handle
 def get_exit():
-    try:
-        user = global_store['user']
-        user.exit()
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+    user = global_store['user']
+    user.exit()
 
     return jsonify({'msg': 'exit success'}), 200
 
