834c4f719085f83213525ec6bfa4e147128d911c
shidenggui <903618848@qq.com>
2018-04-29 16:31:45 +0800

    Bump version: 0.13.11 → 0.13.12

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -5,5 +5,5 @@ from .joinquant_follower import JoinQuantFollower
 from .ricequant_follower import RiceQuantFollower
 from . import exceptions
 
-__version__ = '0.13.11'
+__version__ = '0.13.12'
 __author__ = 'shidenggui'
