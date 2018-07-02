39bcdf343c47235b158970a3bc1ad22f9ecff859
shidenggui <903618848@qq.com>
2018-07-02 22:38:28 +0800

    Bump version: 0.15.0 → 0.15.1

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ return::
 
 setup(
     name="easytrader",
-    version="0.15.0",
+    version="0.15.1",
     description="A utility for China Stock Trade",
     long_description=long_desc,
     author="shidenggui",
