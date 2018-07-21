7b2a27ef92bf845be7c2f0321586b04c96208adf
amos365 <40923243+amos365@users.noreply.github.com>
2018-07-21 14:27:11 +0800

    Update pop_dialog_handler.py

--- easytrader/pop_dialog_handler.py
+++ easytrader/pop_dialog_handler.py
@@ -1,13 +1,10 @@
-# coding:utf-8
-import re
-import time
-
-from . import exceptions
-
-
 class PopDialogHandler:
-    def __init__(self, app):
+    def __init__(self, app, top_window=None):
         self._app = app
+        if top_window is None:
+            self._top_window = self._app.top_window()
+        else:
+            self._top_window = top_window
 
     def handle(self, title):
         if any(s in title for s in {"提示信息", "委托确认", "网上交易用户协议"}):
@@ -27,41 +24,87 @@ class PopDialogHandler:
         for c in range(20):
             sss = time.time()
             try:
-                test = self._app.top_window()
-                test.wait("exists visible enabled", 0.05)
-                return test.Static.window_text()
+                self._top_window.wait("exists visible enabled", 0.05)
+                return self._top_window.Static.window_text()
             except Exception as e:
                 print('_extract_content', e)
             zzz = time.time()
             if (zzz-sss) < 0.05:
                 time.sleep(0.05-(zzz-sss))
-                
-        return test.Static.window_text()
 
     def _extract_entrust_id(self, content):
         return re.search(r"\d+", content).group()
 
     def _submit_by_click(self):
-        self._app.top_window()["确定"].click()
+        for c in range(10):
+            sss = time.time()
+            try:
+                self._top_window["确定"].click()
+                self._top_window.wait_not("exists", 0.1)
+                break
+            except Exception as e:
+                print('PopDialog _submit_by_click', e)
+            zzz = time.time()
+            if((zzz-sss)<0.1):
+                time.sleep(0.1-(zzz-sss))
 
     def _submit_by_shortcut(self):
-        self._app.top_window().type_keys("%Y")
+        for c in range(10):
+            sss = time.time()
+            try:
+                self._top_window.type_keys("%Y")
+                self._top_window.wait_not("exists", 0.1)
+                break
+            except Exception as e:
+                print('PopDialog _submit_by_shortcut', e)
+            zzz = time.time()
+            if((zzz-sss)<0.1):
+                time.sleep(0.1-(zzz-sss))
 
     def _submit_by_shortcut_yes(self):  # 点击 是
-        self._app.top_window().type_keys("%Y")
+        for c in range(10):
+            sss = time.time()
+            try:
+                self._top_window.type_keys("%Y")
+                self._top_window.wait_not("exists", 0.1)
+                break
+            except Exception as e:
+                print('PopDialog _submit_by_shortcut_yes', e)
+            zzz = time.time()
+            if((zzz-sss)<0.1):
+                time.sleep(0.1-(zzz-sss))
 
     def _submit_by_shortcut_no(self):   # 点击 否
-        self._app.top_window().type_keys("%N")
+        for c in range(10):
+            sss = time.time()
+            try:
+                self._top_window.type_keys("%N")
+                self._top_window.wait_not("exists", 0.1)
+                break
+            except Exception as e:
+                print('PopDialog _submit_by_shortcut_no', e)
+            zzz = time.time()
+            if((zzz-sss)<0.1):
+                time.sleep(0.1-(zzz-sss))
         
     def _close(self):
-        self._app.top_window().close()
+        for c in range(10):
+            sss = time.time()
+            try:
+                self._top_window.close()
+                self._top_window.wait_not("exists", 0.1)
+                break
+            except Exception as e:
+                print('PopDialog _close', e)
+            zzz = time.time()
+            if((zzz-sss)<0.1):
+                time.sleep(0.1-(zzz-sss))     
 
 
 class TradePopDialogHandler(PopDialogHandler):
     def handle(self, title):
         if title == "委托确认":
             self._submit_by_shortcut_yes()
-            time.sleep(0.1)
 
         elif title == "提示信息":
             content = self._extract_content()
@@ -73,7 +116,6 @@ class TradePopDialogHandler(PopDialogHandler):
                 return {"failure": content}
             else:
                 self._submit_by_shortcut_yes()
-                time.sleep(0.1)
 
         elif title == "提示":
             content = self._extract_content()
