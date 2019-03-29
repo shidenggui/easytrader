7c908a159b59c177e374f18cc49a714014ffb8f5
lhz <lhztop@163.com>
2019-03-29 08:26:41 +0800

    test

--- easytrader/ht_clienttrader.py
+++ easytrader/ht_clienttrader.py
@@ -47,7 +47,7 @@ class HTClientTrader(clienttrader.BaseLoginClientTrader):
             self._app.top_window().button0.click()
 
             # detect login is success or not
-            self._app.top_window().wait_not("exists", 10)
+            self._app.top_window().wait_not("exists", 100)
 
             self._app = pywinauto.Application().connect(
                 path=self._run_exe_path(exe_path), timeout=10
