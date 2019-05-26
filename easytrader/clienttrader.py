a61681fe81e0a107ce9bc0f728554da2bf88fddb
zhoubeiqing <luoziluojun@126.com>
2019-05-26 09:59:20 +0800

    银河客户端在操作前增加刷新,否则得到的值永远是一样的 (#334)
    * 银河客户端验证码去掉绘制边框
    
    * 银行客户端在操作前增加刷新,否则得到的值永远是一样的

--- easytrader/clienttrader.py
+++ easytrader/clienttrader.py
@@ -339,6 +339,7 @@ class ClientTrader(IClientTrader):
 
     def _switch_left_menus(self, path, sleep=0.2):
         self._get_left_menus_handle().get_item(path).click()
+        self._app.top_window().type_keys('{F5}')
         self.wait(sleep)
 
     def _switch_left_menus_by_shortcut(self, shortcut, sleep=0.5):
