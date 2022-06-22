333c54d1aba3fdc4e94abcf352f99884adbfb2da
wk240 <37340752+wk240@users.noreply.github.com>
2022-06-22 16:16:02 +0800

    Update grid_strategies.py

--- easytrader/grid_strategies.py
+++ easytrader/grid_strategies.py
@@ -113,14 +113,19 @@ class Copy(BaseStrategy):
                     captcha_num = "".join(captcha_num.split())
                     logger.info("captcha result-->" + captcha_num)
                     if len(captcha_num) == 4:
-                        self._trader.app.top_window().window(
-                            control_id=0x964, class_name="Edit"
-                        ).set_text(
-                            captcha_num
-                        )  # 模拟输入验证码
+                        # self._trader.app.top_window().window(
+                        #     control_id=0x964, class_name="Edit"
+                        # ).set_text(
+                        #     captcha_num
+                        # )  # 模拟输入验证码
+                        editor = self._trader.app.top_window().child_window(control_id=0x964, class_name="Edit")
+                        editor.select()
+                        editor.type_keys(captcha_num)
 
                         self._trader.app.top_window().set_focus()
-                        pywinauto.keyboard.SendKeys("{ENTER}")  # 模拟发送enter，点击确定
+                        # pywinauto.keyboard.SendKeys("{ENTER}")  # 模拟发送enter，点击确定
+                        pywinauto.keyboard.send_keys("{ENTER}")  # 模拟发送enter，点击确定
+                        
                         try:
                             logger.info(
                                 self._trader.app.top_window()
