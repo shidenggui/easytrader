c4f26013910b542c54050f724dcabcf4638bf4f7
GQ Mai <gq.mai@aliyun.com>
2024-10-30 21:49:24 +0800

    Add rotating file handler for log

--- easytrader/log.py
+++ easytrader/log.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
+from logging.handlers import TimedRotatingFileHandler
 
 logger = logging.getLogger("easytrader")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 logger.propagate = False
 
 fmt = logging.Formatter(
@@ -11,4 +12,17 @@ fmt = logging.Formatter(
 ch = logging.StreamHandler()
 
 ch.setFormatter(fmt)
+ch.setLevel(logging.INFO)
 logger.handlers.append(ch)
+
+# 创建一个处理器，每天切割日志文件，保留7天
+handler = TimedRotatingFileHandler(
+    "easytrader.log",  # 日志文件名
+    when="midnight",  # 每天的午夜切割
+    interval=1,  # 每隔一天切割
+    backupCount=30,  # 保留7个备份
+    encoding='utf-8'  # 指定编码
+)
+handler.setLevel(logging.DEBUG)
+handler.setFormatter(fmt)
+logger.addHandler(handler)
