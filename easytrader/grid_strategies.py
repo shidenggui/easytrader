2db0dff7941024e196a8862b81ef345a99a41378
Andy <lengyoufang@gmail.com>
2025-12-09 19:32:55 +0800

    fix: 解决多次复制窗口时验证码不验证的问题

--- easytrader/grid_strategies.py
+++ easytrader/grid_strategies.py
@@ -141,7 +141,9 @@ class Copy(BaseStrategy):
                 if not found:
                     self._trader.app.top_window().Button2.click()  # 点击取消
             else:
-                Copy._need_captcha_reg = False
+                pass
+                # 不要将 Copy._need_captcha_reg 置为 False, 因为它是类方法, 一旦置为 False, 后续操作都不再进行验证码识别
+                # Copy._need_captcha_reg = False
         count = 5
         while count > 0:
             try:
